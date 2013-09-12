40beb11f0bba44247285170b936ebf37ab277290
Jeffrey C. Lee <leejeffc@gmail.com>
2013-09-12 16:45:20 -0700

    First commit. I think I'm going in the right direction, but without passing a counter to each recursive call, I have no way of adding the open curly bracket nor having the correct amount of commas!

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,14 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  var keys = Object.keys(obj);
+  if(keys.length){
+  	var value = obj[keys[0]];
+  	delete obj[keys[0]];
+  	return ',"' + keys[0] + '":' + value + stringifyJSON(obj);
+  }
+  else{
+  	return '}';
+  }
 };
+
