61631f9f14790c3bf3499a5ae1f9a1341924a4b2
Jeffrey C. Lee <leejeffc@gmail.com>
2013-09-13 00:06:16 -0700

    Made some progress, but can't run it yet.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,7 +6,29 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
-  // document.body
-  // element.childNodes
-  // element.classList
+
+  // fragment is used to start a new, empty NodeList
+  var fragment = document.createDocumentFragment();
+  var currentNode = (arguments.length > 1) ? arguments[1] : document.body;
+
+  var children = currentNode.childNodes;
+  for(var i = 0; i < children.length; i++){
+    var child = children[i];
+    if(checkClasses(child.classList,className)){
+      fragment.appendChild(child);
+    }
+    if(children[i])
+  }
+
+  return fragment.childNodes;
 };
+
+
+var checkClasses(classes, targetClass){
+  for(var key in classes){
+    if(classes[key] === targetClass){
+      return true;
+    }
+  }
+  return false;
+}
