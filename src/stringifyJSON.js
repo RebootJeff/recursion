c801137b645534c540c63f6470f4518dd9032bda
Jeffrey C. Lee <leejeffc@gmail.com>
2013-09-12 17:27:42 -0700

    I completely misinterpreted the assignment's direction. It's much harder than I originally thought. This commit contains new skeleton, but it's far from complete.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,14 +4,28 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
-  var keys = Object.keys(obj);
-  if(keys.length){
-  	var value = obj[keys[0]];
-  	delete obj[keys[0]];
-  	return ',"' + keys[0] + '":' + value + stringifyJSON(obj);
+  var type = typeof obj;
+  var result;
+  if(Array.isArray(obj)){
+  	for(var i = 0; i < obj.length; i++){
+  		return stringifyJSON(obj[i]);
+  	}
+  }
+  else if(type === 'string'){
+  	return '"' + obj + '"';
+  }
+  else if(type === 'number' || type === 'boolean'){
+  	return obj;
   }
   else{
-  	return '}';
+  	for(var prop in obj){
+  		if(obj.hasOwnProperty(prop)){
+  			var keys = Object.keys(obj);
+  			var value = obj[keys[0]];
+  			delete obj[keys[0]];
+  		}
+  	}
   }
+
 };
 
